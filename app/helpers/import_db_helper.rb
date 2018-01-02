2178170fd2f5e2a9ba1c3f904ea72953b791922b
zvikae <zvi@spectory.com>
2018-01-02 10:26:18 +0200

    sql

--- app/helpers/import_db_helper.rb
+++ app/helpers/import_db_helper.rb
@@ -39,18 +39,11 @@ module ImportDbHelper
     csv = File.read('users.csv')
     values = []
     CSV.parse(csv, headers: true).each do |row|
-      values << "(#{row['first_name']}, #{row['last_name']}, #{row['age']})"
+      values << "('#{row['first_name']}', '#{row['last_name']}', '#{row['email']}', #{row['age']}, now(), now())"
     end
-    arr = values.join(", ")
-    # # c = Student.count
-    # sql = "INSERT INTO users (first_name, last_name, email, age, created_at, updated_at) VALUES #{values}"
-    # # ActiveRecord::Base.connection.execute(sql)
-    # insert into users (first_name, last_name, email, age, created_at, updated_at)
-    # values ('aa', 'bb', 'zvi@dsd.com', 33, now(), now()),
-    # ('a4', 'bb', 'zvi@dsd.com', 33, now(), now()),
-    # ('65', 'bb', 'zvi@dsd.com', 33, now(), now()),
-    # ('63', 'bb', 'zvi@dsd.com', 33, now(), now())
-
+    values_array = values.join(', ')
+    sql = "INSERT INTO users (first_name, last_name, email, age, created_at, updated_at) VALUES #{values_array}"
+    ActiveRecord::Base.connection.execute(sql)
     end_time = Time.zone.now
     ap 'import_users_bulk ran: ' + ((end_time - start_time).round(2)).to_s + ' second!'
   end

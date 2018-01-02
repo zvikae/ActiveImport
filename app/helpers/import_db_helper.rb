1f857a2b92ec2eea92e1a9f147b2deec0fe69c95
zvikae <zvi@spectory.com>
2018-01-02 10:10:26 +0200

    changes

--- app/helpers/import_db_helper.rb
+++ app/helpers/import_db_helper.rb
@@ -37,12 +37,20 @@ module ImportDbHelper
   def self.import_users_bulk_sql
     start_time = Time.zone.now
     csv = File.read('users.csv')
+    values = []
     CSV.parse(csv, headers: true).each do |row|
-      values = row.map { |i| "('fname_#{i}', 'lname_#{i}', #{AGE_RANGE.sample})" }.join(', ')
+      values << "(#{row['first_name']}, #{row['last_name']}, #{row['age']})"
     end
-    # c = Student.count
-    # sql = "INSERT INTO students (first_name, last_name, age) VALUES #{values}"
-    # ActiveRecord::Base.connection.execute(sql)
+    arr = values.join(", ")
+    # # c = Student.count
+    # sql = "INSERT INTO users (first_name, last_name, email, age, created_at, updated_at) VALUES #{values}"
+    # # ActiveRecord::Base.connection.execute(sql)
+    # insert into users (first_name, last_name, email, age, created_at, updated_at)
+    # values ('aa', 'bb', 'zvi@dsd.com', 33, now(), now()),
+    # ('a4', 'bb', 'zvi@dsd.com', 33, now(), now()),
+    # ('65', 'bb', 'zvi@dsd.com', 33, now(), now()),
+    # ('63', 'bb', 'zvi@dsd.com', 33, now(), now())
+
     end_time = Time.zone.now
     ap 'import_users_bulk ran: ' + ((end_time - start_time).round(2)).to_s + ' second!'
   end
